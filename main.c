8dcf091a9c0f5c5466e2e1d7111177b968f01bc5
Payden McBee <c15payden.mcbee@usafa.edu>
2013-12-05 18:54:46 -0700

    added main.c file

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <msp430g2553.h>
+#include "Robot_Motions/robotMotion.h"
+#include "RobotSensing/sensor.h"
+
+
+void main(void) {
+    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+	
+
+}
